a821e768c4b82760615eb208da72007ce49f19e6
Komal Essarani <42622695+komalessarani@users.noreply.github.com>
2020-07-29 17:46:56 -0400

    Add files via upload

--- src/contents/Project.js
+++ src/contents/Project.js
@@ -7,7 +7,7 @@ function Project(){
    <div id="project" className="container">
         <div style={{position:'relative', width: '100%'}}>
             <h1 style={{color: 'black', fontSize:40}}>My Projects</h1>   <br/>  
-              <div className="card" onClick={window.open("/2048")} style={{width:'80%'}}>
+              <div className="card" style={{width:'80%'}}>
                 <div className="card-image">
                   <figure className="image">
                     <img src={require('../assets/2048pic.png')} alt="2048"/>
